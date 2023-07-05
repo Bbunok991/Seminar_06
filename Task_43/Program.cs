068ee09337cc6fbc8a39758497deda79997a3664
Danila <Bbunok991@yandex.ru>
2023-07-05 22:48:38 +0300

    Completed the task 43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
 void Main()
 {
-    int k1 = InPut("Enter the lenght of the array: "); // Количество чисел всего.
-    int b1 = InPut("Enter the lenght of the array: "); // Количество чисел всего.
-    int k2 = InPut("Enter the lenght of the array: "); // Количество чисел всего.
-    int b2 = InPut("Enter the lenght of the array: "); // Количество чисел всего.
-    int numbers = Numbers(quantity); // Получение результата функции "Numbers" в переменную "numbers".
-    System.Console.WriteLine(numbers); // Вывод количества чисел больше 0.
+    int b1 = InPut("Enter the b1: "); //Ввод значения b1
+    int k1 = InPut("Enter the k1: "); //Ввод значения k1
+    int b2 = InPut("Enter the b2: "); //Ввод значения b2
+    int k2 = InPut("Enter the k2: "); //Ввод значения k2
+    string directs = Directs(k1, b1, k2, b2); // Получение результата функции "Directs" в переменную "directs".
+    System.Console.WriteLine(directs); // Вывод количества значений b1, k1, b2 и k2.
 }
 int InPut(string text) //Функция для ввода числа.
 {
@@ -14,17 +15,17 @@ int InPut(string text) //Функция для ввода числа.
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int Numbers(int quantity) // Функция для заполнения массива и определения количества четных чисел в массиве.
+string Directs(int k1, int b1, int k2, int b2) // Функция, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2
 {
-    int sum = 0; //Количество чисел больше 0.
-    for(int i = 0; i < quantity; i++) // Цикл для ввода чисел и заполнения переменной "sum".
-    {
-        System.Console.Write("Enter the number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        if(number > 0) sum++; //Проверка условия.
-    }
-    return sum; // Возвращение количества чисел больше 0.
+    int[] Y1 = new int[2] {k1, b1};
+    int[] Y2 = new int[2] {k2, b2};
 
+    double first_direct = (double) (-1 *(Y1[1] - Y2[1])) / (Y1[0] - Y2[0]);
+    double second_direct = (double) (Y2[0] * first_direct + Y2[1]);
+    
+    string str = $"({first_direct};{second_direct})";
+
+    return str;
 }
 
 Main(); //Вызов функции "Main".
